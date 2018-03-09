49b7e355ae1859eb38e38cdef599391be96bc45f
Robert Hätte <35559997+RobeHat@users.noreply.github.com>
2018-03-09 13:28:04 +0100

    test Commit IDEA

--- primeNG/src/app/app.module.ts
+++ primeNG/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import { ButtonsComponent } from './buttons/buttons.component';
+import {primeNgModule} from "./primeng.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    primeNgModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
